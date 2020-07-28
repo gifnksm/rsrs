bfa90e0920458c32a288133478bfc3a61255c9ef
NAKASHIMA, Makoto <makoto.nksm+github@gmail.com>
2020-07-29 00:04:07 +0900

    get login shell from /etc/passwd

--- src/bin/rsrs-remote.rs
+++ src/bin/rsrs-remote.rs
@@ -1,3 +1,4 @@
+use etc_passwd::Passwd;
 use std::{env, ffi::OsString, os::unix::process::CommandExt, process::Command as StdCommand};
 use tokio::{prelude::*, process::Command};
 use tokio_pty_command::{CommandExt as _, PtyMaster};
@@ -8,9 +9,16 @@ type Result<T> = std::result::Result<T, Error>;
 #[tokio::main]
 async fn main() -> Result<()> {
     env::set_var("RUST_BACKTRACE", "1");
-
     // FIXME: set TERM envvar
-    let shell = env::var_os("SHELL").unwrap();
+
+    let shell = if let Some(passwd) = Passwd::current_user()? {
+        OsString::from(passwd.shell.to_str()?)
+    } else if let Some(shell) = env::var_os("SHELL") {
+        shell
+    } else {
+        panic!("cannot get login shell for the user");
+    };
+
     let mut arg0 = OsString::from("-");
     arg0.push(&shell);
 
